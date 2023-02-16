98b15635ba1b199031eb1d25c33703c486a45f9e
Samson <sammossallam@gmail.com>
2023-02-15 19:27:51 -0500

    user

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
-const User = require("../models/user");
+
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -18,13 +18,22 @@ userSchema.methods.generateAuthToken = function () {
 };
 
 const validate = (data) => {
+  const complexityOptions = {
+    min: 6,
+    max: 30,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 1,
+  };
+
   const schema = joi.object({
     firstName: joi.string().required().label("First Name"),
     lastName: joi.string().required().label("Last Name"),
     email: joi.string().email().required().label("Email"),
-    password: passwordComplexity.required().label("Password"),
+    password: passwordComplexity(complexityOptions),
   });
   return schema.validate(data);
 };
 
-module.exports = (User, validate);
+module.exports = { User: mongoose.model("User", userSchema), validate };
