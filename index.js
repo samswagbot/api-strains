3513d651809acffa857f15624b4dc2c7edb06768
Samson <sammossallam@gmail.com>
2023-02-15 19:58:59 -0500

    fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use("/api/users", userRoutegit s);
+app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/strains", strainRoutes);
 
