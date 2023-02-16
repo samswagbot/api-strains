e86def2d4c667a3227d1b54674379c10369b25ba
Samson <sammossallam@gmail.com>
2023-02-16 13:43:10 -0500

    fixing cors issue

--- index.js
+++ index.js
@@ -16,14 +16,12 @@ connection();
 // middleware
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: [
-      "https://sams-cannabis-store.netlify.app/",
-      "http://localhost:3000/",
-    ],
-  })
-);
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
 
 
 app.use("/api/users", userRoutes);
